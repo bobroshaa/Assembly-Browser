b3c37636c2b597c9f41b94b74bad3c11ec8a7831
bobroshaa <bobroshaa@mail.ru>
2022-11-04 11:17:44 +0300

    add model from mvvm

---
+++ Assembly-Browser/AssemblyBrowser.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Assembly_Browser
+{
+    public class AssemblyBrowser : INotifyPropertyChanged
+    {
+        private string _path;
+
+        public string Path
+        {
+            get { return _path;}
+            set
+            {
+                _path = value;
+                OnPropertyChanged("Path");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
